59fe7508d2fbe0964fb07a4eb0f877cb2728dc23
Cyril CONCAS <concascyril2@hotmail.com>
2024-10-18 16:32:33 +0200

    Init Repo

---
+++ app.py
@@ -0,0 +1,14 @@
+import streamlit as st
+import numpy as np
+
+st.write("Hello World")
+st.write("Hello World")
+
+st.button("Test")
+
+st.snow()
+st.balloons()
+
+
+
+st.write(np.random.randint(1,100))
